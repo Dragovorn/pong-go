bf036a97f315264de4c4d2564156ad033025e813
Andrew Burr <me@dragovorn.com>
2021-10-24 11:47:10 -0400

    implement sakura log file generation code

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/dragovorn/go-pong/pong"
 	"github.com/isshoni-soft/sakura"
+	"github.com/isshoni-soft/sakura/logging"
 )
 
 func main() {
+	logging.InitLogfile("pong/logs/")
 	sakura.Init(pong.Init())
 }
